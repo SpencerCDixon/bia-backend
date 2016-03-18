4d9e15ee11868b281a1259463fb00b3963c36525
Spencer Dixon <spencercdixon@gmail.com>
2016-03-18 16:34:03 -0400

    Npm version

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var monk = require('monk');
 var DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/bia';
 
 var config = {
-  PORT: process.env.port || 4200,
+  PORT: process.env.PORT || 4200,
   DB_URL: DATABASE_URL,
   db: monk(DATABASE_URL)
 };
